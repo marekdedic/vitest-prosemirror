e9db09f7a88d6e30e43e0d6dcbf403d0e55b6abd
Marek Dědič <developer@dedic.eu>
2025-02-28 12:52:42 +0100

    Added basic build config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: "src/index",
+      formats: ["es"],
+      name: "vitest-prosemirror",
+    },
+    minify: false,
+    rollupOptions: {
+      external: [
+        "prosemirror-model",
+        "prosemirror-state",
+        "prosemirror-view",
+        "stringify-object",
+        "test-keyboard",
+        "vitest",
+      ],
+    },
+    sourcemap: true,
+  },
+  plugins: [dts({ rollupTypes: true })],
+});
